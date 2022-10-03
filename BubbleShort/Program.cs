b4b3adde1e719f17778aa7fd875266ea2cb3dbe6
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-10-03 22:30:30 +0700

    class

--- BubbleShort/Program.cs
+++ BubbleShort/Program.cs
@@ -1,15 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 
-namespace BubbleShort
+namespace Buble_Sort
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+        //Fungsi / method untuk menerima masukan
+        public void read()
         {
+            //Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array:");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+                Console.WriteLine("");
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Masukkan elemen array");
+                Console.WriteLine("---------------------");
+
+                //Pengguna memasukkan elemen pada array
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("<" + (i + i) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[i] = Int32.Parse(s1);
+                }
+
+            }
         }
+        public void display()
+        {
+            //Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Element array yang telah tersusun");
+            Console.WriteLine("---------------------------------");
+            for (int j = 0; j < n; j++) ;
+            {
+                Console.WriteLine("");
+            }
+
+        }
+
+        public void BubbleSortArray()
+        {
+
+
+            for (int i = 1; i < n; i++) //For n - 1 passes
+            {
+                //Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) //Jika elemen tidak dalam urutan yang benar
+                    {
+                        //Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+
+
+                    }
+                }
+            }
+        }
+    
+         static void Main(string[] args)
+         {
+                //Creating the object of the BubbleSort class
+                Program myList = new Program();
+                //Pemanggilan fungsi untuk menerima elemen array
+                myList.read();
+                //Pemanggilan fungsi untuk mengurutkan array
+                myList.BubbleSortArray();
+                //Pemanggilan fungsi untuk menampilkan array yang tersusun
+                myList.display();
+                //Exit
+                Console.WriteLine("\n\nTekan Tombol Apa Saja Untuk Keluar.");
+                Console.Read();
+           
+        
+         }
+
     }
+
 }
